47512f8bbd02d6a11bb1b3082d1670212a5c5679
b1rtek <skrzeczek@onet.pl>
2022-03-01 09:01:59 +0100

    Console version now uses the Genius class instead of its own outdated code

---
+++ main-cli.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+#include "Genius.h"
+
+using namespace std;
+
+void printVectorContents(vector<string> content, string title = "", string footer = "") {
+    if (!title.empty()) cout << title << endl;
+    for (auto &line: content) {
+        cout << line << endl;
+    }
+    if (!footer.empty()) cout << footer;
+}
+
+bool yesOrNo() {
+    string input;
+    getline(cin, input);
+    return (input.empty() || input[0] == 'Y' || input[0] == 'y');
+}
+
+int main(int argc, char **argv) {
+    int wordSize = 5;
+    if (argc >= 2) {
+        wordSize = stoi(argv[1]);
+    }
+    Genius genius = Genius();
+    genius.start("default");
+    genius.setWordSize(wordSize);
+    vector<string> suggestedStartWords = genius.getStarters();
+    printVectorContents(suggestedStartWords, "Suggested start words: ");
+    string userWord, result;
+    while (true) {
+        cout << "Enter your word: ";
+        cin >> userWord;
+        cout << "Enter the result: ";
+        cin >> result;
+        genius.enterWord(userWord, result);
+        genius.analyze();
+        vector<string> matchingWords = genius.getMatching();
+        int matchingAmount = genius.getMatchingAmount();
+        if (matchingAmount == 1) {
+            cout << "The word is " << matchingWords[0] << endl;
+            break;
+        } else {
+            cout << "Found " << matchingAmount << " matching words. Display them? [Y/n]";
+        }
+        getline(cin, result); /// garbage
+        if (yesOrNo()) {
+            printVectorContents(matchingWords, "Matching words:");
+        }
+        vector<string> dataWords = genius.getDataWords();
+        printVectorContents(dataWords, "Suggested words:", "Display more? [Y/n]");
+        while (yesOrNo()) {
+            dataWords = genius.getDataWords();
+            printVectorContents(dataWords, "", "Display more? [Y/n]");
+        }
+    }
+    return 0;
+}
